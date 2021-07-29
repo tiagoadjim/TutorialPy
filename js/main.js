cfd1785221f9fa90056b1d6a675382359d2b0ff0
tiagoadjimmm <tiagoadjimbeats@gmail.com>
2021-07-29 19:40:35 -0300

    se agrego un for para practicar el uso del mismo!

--- js/main.js
+++ js/main.js
@@ -23,14 +23,26 @@ function preguntas(){
     // if (acepta == 'si' || 'Si')
 
     if (acepta == 'si' || acepta == 'Si') {
-        alert("Listo! Continue para ser redirigido!");
+
+        alert("Listo! Aceptaste los T&C! Continue para ser redirigido!");
         console.log("Listo! " + nombre + " Esta siendo Rick Roleado!" );
-        window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+        // window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+        captcha();
 
         return acepta;
     } else {
         alert("Queda cancelado el tramite por la inaceptacion de los terminos y condiciones.");
        console.log("Se cancelo el tramite y se evito el rick roleo");
+       while (acepta !== 'si' || acepta !== 'Si' || acepta !== 'SI') {
+        acepta = prompt("Debe aceptar los terminos para poder proceder. Responda Si para poder continuar o bien cierre el sitio.");
+    }
     }
 
+}
+
+
+function captcha(){
+    for (var i = 0; i < 10; i++){
+        alert("Haga click en continuar " + i + "  Veces para validar que usted No es un robot.");
+    }
 }
